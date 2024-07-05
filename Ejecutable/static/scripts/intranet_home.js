fefd03e5087be4f8098dfdeff0368ab64e30e953
DominickPortella <157386679+DominickPortella@users.noreply.github.com>
2024-07-05 13:14:06 -0500

    new

---
+++ Ejecutable/static/scripts/intranet_home.js
@@ -0,0 +1,19 @@
+var images = document.querySelectorAll(".image-container img");
+var currentIndex = 0;
+function changeImage(index) {
+    images[currentIndex].style.display = "none";
+    currentIndex = index;
+    images[currentIndex].style.display = "block";
+    updateButtonStyle();
+}
+function updateButtonStyle() {
+    var buttons = document.querySelectorAll(".image-controls button");
+    buttons.forEach(function (button) {
+        button.classList.remove("active");
+    });
+    buttons[currentIndex].classList.add("active");
+}
+images[currentIndex].style.display = "block";
+setInterval(function () {
+    changeImage((currentIndex + 1) % images.length);
+}, 5000);
