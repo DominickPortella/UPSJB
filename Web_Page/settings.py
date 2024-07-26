abb43ae72e66127c747f7216a1e73623e2cb8561
DominickPortella <157386679+DominickPortella@users.noreply.github.com>
2024-07-26 13:12:35 -0500

    new

--- Web_Page/settings.py
+++ Web_Page/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Web_Page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '/Ejecutable/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
