0fabbc6e1d3697a8242da146b645f35d30cc0d89
leonardo22Chavesta <146782802+leonardo22Chavesta@users.noreply.github.com>
2024-12-29 01:12:12 -0500

    init

--- Web_Page/settings.py
+++ Web_Page/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-98zt-5a1^j%oojn4y-ah%wf_@69eh5%4y30xw8(gefvo@mg=9&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
